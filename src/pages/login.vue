b96e9a2efca77510a0b0565f5aea224f0d4a2d1c
小忆 <10949408+kkooppooppookkkj@user.noreply.gitee.com>
2024-08-09 21:51:01 +0800

    8.9 1

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -3,14 +3,18 @@
     <div class="login-container">
         <img class="login-image" src="../assets/login/login_back.png" alt="">
         <div>
+          <login-password></login-password>
+        </div>
+        <!-- <div>
             <div>欢迎登录</div>
             <bwy-button>登录</bwy-button>
-        </div>
+        </div> -->
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import LoginPassword from '../templates/login/LoginPassword.vue'
 import '@/utils/colourBar.js'
 </script>
 
---
+++ src/templates/login/LoginPassword.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="login-password">
+    <!-- -->
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss"></style>
